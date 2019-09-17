da354f02a105a15b11ce15142d1164d75b9d115b
sbtries <sarahbtracy17@gmail.com>
2019-09-16 20:22:43 -0700

    Updates

---
+++ precache-manifest.f3d6c406c8426fe45155e01bbba14100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089d6983933d4d73adf68bfdb732597f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c248ed1a3d7636e1347f",
+    "url": "/portfolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1a12d4307bac8a52edb4",
+    "url": "/portfolio/static/css/main.e9dd970e.chunk.css"
+  },
+  {
+    "revision": "c248ed1a3d7636e1347f",
+    "url": "/portfolio/static/js/2.40f68b51.chunk.js"
+  },
+  {
+    "revision": "1a12d4307bac8a52edb4",
+    "url": "/portfolio/static/js/main.44215197.chunk.js"
+  },
+  {
+    "revision": "5029282666060c428c88",
+    "url": "/portfolio/static/js/runtime~main.cbff171f.js"
+  },
+  {
+    "revision": "192fc589c7e46f0e919ea9cf98c7e84c",
+    "url": "/portfolio/static/media/profile.192fc589.jpg"
+  }
+]);
